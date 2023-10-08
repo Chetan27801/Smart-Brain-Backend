ad9e4df108c5fc4d0ea35faeac67e12a892b2131
Chetan <c27801j@gmail.com>
2023-10-08 17:41:03 +0530

    again serverjs

--- server.js
+++ server.js
@@ -10,18 +10,19 @@ const image = require("./controllers/image");
 const db = knex({
 	client: "pg",
 	connection: {
-		connectionString: process.env.DATABASE_URL,
-		ssl: {rejectUnauthorized: false},
-		host: process.env.DATABASE_URL,
-		port: 5432,
-		user: process.env.DATABASE_USER,
-		password: process.env.DATABASE_PW,
-		database: process.env.DATABASE_DB,
+		// connectionString: process.env.DATABASE_URL,
+		// ssl: {rejectUnauthorized: false},
+		// host: process.env.DATABASE_URL,
+		// port: 5432,
+		// user: process.env.DATABASE_USER,
+		// password: process.env.DATABASE_PW,
+		// database: process.env.DATABASE_DB,
 
-		// host: "127.0.0.1",
-		// user: "postgres",
-		// password: "test",
-		// database: "smart-brain",
+		host: "127.0.0.1",
+		port: 5432,
+		user: "postgres",
+		password: "test",
+		database: "smart-brain",
 	},
 });
 
